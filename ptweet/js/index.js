ae26c10134e10de872cbc0c0587fe109db17359d
omar07hdez <57792624+omar07hdez@users.noreply.github.com>
2021-11-10 00:24:55 -0700

    ptweet

--- ptweet/js/index.js
+++ ptweet/js/index.js
@@ -1,5 +1,51 @@
 window.onload=()=>{
     let usuario= JSON.parse(localStorage.getItem("usuario"));
-    console.log(usuario);
-    document.getElementById("usuario").innerHTML="Hola "+usuario.nombre;
-}
+    document.getElementById("imgUser").src="./img/"+usuario.imagen;
+    let arrayTw=[];
+    //console.log(usuario);
+    //document.getElementById("usuario").innerHTML="Hola "+usuario.nombre;
+    let txtTweet=document.getElementById("txtTweet")
+    let btnTweet=document.getElementById("btnTweet")
+    btnTweet.addEventListener('click',(evt)=>{
+        if(txtTweet.value.trim() != ""){
+            let obj = {
+                img:usuario.imagen,
+                nombre:usuario.nombre,
+                mensaje:txtTweet.value,
+                username:usuario.username,
+            };
+
+            arrayTw.push(obj);
+            crearPost()
+            txtTweet.value="";
+
+
+        }
+       
+    });
+    txtTweet.addEventListener('keyup',(evt)=>{
+
+    });
+    function crearPost(){
+        var todo="";
+        arrayTw.forEach(el=>{
+            todo+=`<div class="post">
+            <div>
+                <img src="./img/${el.img}" alt="" class="imgUser">
+            </div>
+            <div>
+                <h2> 
+                    <span>${el.nombre}</span>
+                    <span>@${el.username}</span>
+                </h2>
+                <div class="textTweet">
+                    ${el.mensaje}
+                </div>
+            </div>
+        </div>`;
+        });
+        document.getElementById("posts").innerHTML=todo;
+    
+
+    }
+};
